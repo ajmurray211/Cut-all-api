1ab9cb6469dc7c30094a3777fbde7a3ad65d35f9
AJ Murray <aj.murr4y@gmail.com>
2022-08-23 17:19:47 -0400

    added routes for the url and registed site in admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from api.models.parts import Part
+from api.models.worker import Worker
 
 # Register your models here.
+
+admin.site.register(Part)
+admin.site.register(Worker)
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import URLPattern, path
 from .views.parts import PartsView, PartView
+from .views.worker import WorkersView, WorkerView
 
 urlpatterns = [
     path('parts/', PartsView.as_view(), name='parts'),
     path('parts/<int:pk>', PartView.as_view(), name='part'),
+    path('workers/', WorkersView.as_view(), name='workers'),
+    path('workers/<int:pk>', WorkerView.as_view(), name='worker'),
 ]
